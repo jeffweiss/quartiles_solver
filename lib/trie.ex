a8a47071a3d0558133d891f616cf98dc1a9f1c4d
Jeff Weiss <jeff.a.weiss@gmail.com>
2025-05-19 02:28:44 -0700

    clean up trie variable names

--- lib/trie.ex
+++ lib/trie.ex
@@ -3,34 +3,31 @@ defmodule QuartilesSolver.Trie do
   Modified version of Okasaki's Trie, Purely Functional Data Structures, page 165
   """
 
-  @type trie() :: {:none, Map.t()} | {{:some, String.t()}, Map.t()}
   @type search_path() :: charlist()
-  @type payload() :: String.t()
+  @type payload() :: any()
+  @type value() :: :none | {:some, payload()}
+  @type trie() :: {value(), Map.t()}
 
   @empty {:none, %{}}
 
   @spec lookup(trie(), String.t() | search_path()) :: :not_found | payload()
   def lookup(trie, word) when is_binary(word), do: lookup(trie, String.to_charlist(word))
   def lookup({:none, _}, []), do: :not_found
-  def lookup({{:some, x}, _}, []), do: x
-  def lookup({_trie, m}, [k | ks]), do: lookup(Map.get(m, k, @empty), ks)
+  def lookup({{:some, payload}, _}, []), do: payload
+  def lookup({_trie, children}, [key | rest]), do: lookup(Map.get(children, key, @empty), rest)
 
   @spec bind(trie(), String.t() | search_path(), payload()) :: trie()
   def bind(trie, path, payload) when is_binary(path) do
     bind(trie, String.to_charlist(path), payload)
   end
 
-  def bind({_, m}, [], x), do: {{:some, x}, m}
+  def bind({_, children}, [], payload), do: {{:some, payload}, children}
 
-  def bind({v, m}, [k | ks], x) do
-    t =
-      case Map.get(m, k) do
-        nil -> @empty
-        other -> other
-      end
+  def bind({value, children}, [key | rest], payload) do
+    t = Map.get(children, key, @empty)
 
-    t_prime = bind(t, ks, x)
-    {v, Map.put(m, k, t_prime)}
+    t_prime = bind(t, rest, payload)
+    {value, Map.put(children, key, t_prime)}
   end
 
   def new, do: @empty
